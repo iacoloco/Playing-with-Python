596e40128f27aa37cf5a621dfedb92b2f2d4ed9d
iacoloco <iachiniarmando@gmail.com>
2024-03-20 17:37:09 +0000

    Class Employee

---
+++ Classes.py
@@ -0,0 +1,24 @@
+class Employee:
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + "." + last + "@company.com"
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+emp_1= Employee('Corey','Schafer',  50000 )
+emp_2= Employee('Test','User',  60000 )
+
+#or
+
+print('{} {}'.format(emp_1.first, emp_1.last))
+
+#or
+print(emp_1.fullname())
+
+
+#or
+print(Employee.fullname(emp_1))
